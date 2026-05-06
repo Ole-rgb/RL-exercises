a4a86008fa56e73c12fc1d5a23c9653c163e05d0
Sohan <me.sohandeshar+ghhp460@gmail.com>
2026-05-06 08:56:42 +0200

    wip: Level 3

---
+++ rl_exercises/week_4/level_3/__init__.py
@@ -0,0 +1,6 @@
+"""Level 3 week 4 extensions."""
+
+from rl_exercises.week_4.level_3.extended_dqn import ExtendedDQNAgent
+from rl_exercises.week_4.level_3.per import PrioritizedReplayBuffer, SumTree
+
+__all__ = ["ExtendedDQNAgent", "PrioritizedReplayBuffer", "SumTree"]
